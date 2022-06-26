96962de77cc88a814027ab313715ad21a2f42be5
JayGalvan90 <39308108+JayGalvan90@users.noreply.github.com>
2022-06-26 11:03:03 -0400

    Initial coin offering
    connected with NFT-Collection which is then connected to the whitelist.

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x5De4243677b100e9c0e9A47baA97e606A396F4Ae";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
